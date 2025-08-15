957c34020e432f55de0db5a72a65b19e6abc666d
Sven Hagemann-Kraan <sven@savagetiger.org>
2025-08-15 16:16:38 +0200

    valkey password support

--- maerquin-v3/app/cache.php
+++ maerquin-v3/app/cache.php
@@ -6,8 +6,16 @@ use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder): void {
     $valkeyHostname = (string)$_ENV['VALKEY_HOSTNAME'];
+    $valkeyPassword = (string)$_ENV['VALKEY_PASSWORD'];
 
-    $client = new Predis\Client(sprintf('tcp://%s', $valkeyHostname));
+    $client = new Predis\Client(
+        [
+            'scheme' => 'tcp',
+            'host' => $valkeyHostname,
+            'password' => $valkeyPassword,
+            'port' => 6_379,
+        ],
+    );
     $cache = new Symfony\Component\Cache\Adapter\RedisAdapter($client);
 
     $containerBuilder->addDefinitions([
